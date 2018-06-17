b36728fe88801701e3121ea4f3a616caf62226f0
Leonardo Xool <leonsio_xool@hotmail.com>
2018-06-17 17:43:51 -0500

    jjj

--- IntegratorProject3ero/AppDesktop/GUI/newAsiento.Designer.cs
+++ IntegratorProject3ero/AppDesktop/GUI/newAsiento.Designer.cs
@@ -97,7 +97,7 @@
             this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnEliminar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.btnEliminar.ForeColor = System.Drawing.Color.White;
-            this.btnEliminar.Location = new System.Drawing.Point(347, 21);
+            this.btnEliminar.Location = new System.Drawing.Point(332, 21);
             this.btnEliminar.Name = "btnEliminar";
             this.btnEliminar.Size = new System.Drawing.Size(90, 36);
             this.btnEliminar.TabIndex = 58;
